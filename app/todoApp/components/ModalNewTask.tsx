13ac84917628cbfc5e92332a2dc3a90e6c4816c6
Wasn <116284029+HaimiyaWasn@users.noreply.github.com>
2026-07-12 14:08:03 +0700

    Update code

--- app/todoApp/components/ModalNewTask.tsx
+++ app/todoApp/components/ModalNewTask.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useEffect } from "react";
+import { FaTimes } from "react-icons/fa";
 
 type ModalNewTaskProps = {
   openModal: boolean;
@@ -38,10 +39,23 @@ export default function ModalNewTask({
     >
       <div
         onClick={(e) => e.stopPropagation()}
+        className={`w-full max-w-2xl mx-5 p-5 rounded-2xl bg-sky-500 max-h-[90vh] transform transition-all duration-300  ${
+          openModal
+            ? "scale-100 opacity-100 translate-y-0"
+            : "scale-95 opacity-0 translate-y-5"
+        }`}
       >
-        <h1>
-          Add New Task
-        </h1>
+        <div className="flex items-center justify-between p-3">
+          <h1 className="text-lg md:text-2xl font-extrabold">
+            Add New Task
+          </h1>
+          <button
+            onClick={() => closeModal()}
+            className="btn btn-circle hover:opacity-75 active:opacity-75 transition-opacity duration-300"
+          > 
+            <FaTimes size={20} />
+          </button>
+        </div>
       </div>
     </div>
   )
